d685b266e34965e07d4e13226489b01a4bd16110
Yashasvi <kmryashasvi@gmail.com>
2024-03-11 12:16:58 +0530

    first

---
+++ app.js
@@ -0,0 +1,120 @@
+function openPageWithDelay(pageURL) {
+    setTimeout(function () {
+      window.location.href = pageURL;
+    }, 500); // 500 milliseconds delay
+  }
+  function about() {
+    openPageWithDelay("about.html");
+  }
+  function compiler() {
+    openPageWithDelay("compiler.html");
+  }
+  
+  function binarysrch() {
+    openPageWithDelay("binarysearch.html");
+  }
+  
+  function count() {
+    openPageWithDelay("count.html");
+  }
+  
+  function dqueue() {
+    openPageWithDelay("dqueue.html");
+  }
+  
+  function forwardList() {
+    openPageWithDelay("forwardList.html");
+  }
+  
+  function isSorted() {
+    openPageWithDelay("isSorted.html");
+  }
+  
+  function isSorUntil() {
+    openPageWithDelay("isSorUntil.html");
+  }
+  
+  function iterators() {
+    openPageWithDelay("iterators.html");
+  }
+  
+  function list() {
+    openPageWithDelay("list.html");
+  }
+  
+  function map_multimap() {
+    openPageWithDelay("map_multimap.html");
+  }
+  
+  function merge() {
+    openPageWithDelay("merge.html");
+  }
+  
+  function minmax() {
+    openPageWithDelay("minmax.html");
+  }
+  
+  function multiset() {
+    openPageWithDelay("multiset.html");
+  }
+  
+  function pair() {
+    openPageWithDelay("pair.html");
+  }
+  
+  function parsort() {
+    openPageWithDelay("parsort.html");
+  }
+  
+  function popcount() {
+    openPageWithDelay("popcount.html");
+  }
+  
+  function queue() {
+    openPageWithDelay("queue.html");
+  }
+  
+  function rev() {
+    openPageWithDelay("rev.html");
+  }
+  
+  function reverseCopy() {
+    openPageWithDelay("reverseCopy.html");
+  }
+  
+  function rotate() {
+    openPageWithDelay("rotate.html");
+  }
+  
+  function set() {
+    openPageWithDelay("set.html");
+  }
+  
+  function sorting() {
+    openPageWithDelay("sorting.html");
+  }
+  
+  function stack() {
+    openPageWithDelay("stack.html");
+  }
+  
+  function templateClass() {
+    openPageWithDelay("templateClass.html");
+  }
+  
+  function templateFunction() {
+    openPageWithDelay("templateFunction.html");
+  }
+  
+  function unordered_map() {
+    openPageWithDelay("unordered_map.html");
+  }
+  
+  function unordered_set() {
+    openPageWithDelay("unordered_set.html");
+  }
+  
+  function vector() {
+    openPageWithDelay("vector.html");
+  }
+  
